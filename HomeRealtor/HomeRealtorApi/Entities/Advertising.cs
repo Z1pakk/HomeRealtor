95676dd70287e436cd68c0e7a890d6aa6c5ff1dd
angelina <angelyusha.u@gmail.com>
2019-10-15 15:35:56 +0300

    Add advertising and add properties in else

---
+++ HomeRealtor/HomeRealtorApi/Entities/Advertising.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeRealtorApi.Entities
+{
+    [Table("tbl_Advertisings")]
+    public class Advertising
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Image { get; set; }
+        [Required] public string StateName { get; set; }
+        [Required] public double Price { get; set; }
+        [Required] public string Contacts { get; set; }
+
+        [ForeignKey("UserOf")]
+        public int UserId { get; set; }
+        public virtual User UserOf { get; set; }
+    }
+}
