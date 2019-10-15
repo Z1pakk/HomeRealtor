080b7ae72356577e00a44ae3fe9281147c40945a
kozack <kozack@A33PC13>
2019-10-15 17:12:41 +0300

    started real estate controller

---
+++ HomeRealtor/HomeRealtorApi/Controllers/RealEstateController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeRealtorApi.Entities;
+using HomeRealtorApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeRealtorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RealEstateController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("get/realEstate={id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost("add/realEstate")]
+        public ContentResult AddRealEstate([FromBody]RealEstateViewModel model)
+        {
+            try
+            {
+                RealEstate estate = new RealEstate()
+                {
+                    Active = model.Active,
+                    Image = model.Image,
+                    ImageEstates = model.ImageEstates,
+                    Price = model.Price,
+                    StateName = model.StateName,
+                    TypeId = model.TypeId,
+                    UserId = model.UserId,
+                    TimeOfPost = model.TimeOfPost
+                };
+               
+                return Content("Real Estate is added");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error" + ex.Message);
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
