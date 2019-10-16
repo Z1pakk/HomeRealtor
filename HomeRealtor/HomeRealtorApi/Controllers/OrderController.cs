f5f5ab2929680f1b82efad95f806e921844981e3
vetalkasmile <vetalkasmile@gmail.com>
2019-10-16 17:45:11 +0300

    someChanges

---
+++ HomeRealtor/HomeRealtorApi/Controllers/OrderController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeRealtorApi.Entities;
+using HomeRealtorApi.Helpers;
+using HomeRealtorApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HomeRealtorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly EFContext _context;
+        private readonly IHostingEnvironment _appEnvironment;
+        public OrderController(EFContext context, IHostingEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        [HttpGet("orders")]
+        public ContentResult getOrders()
+        {
+            List<Order> orders = _context.Orders.ToList();
+            string json = JsonConvert.SerializeObject(orders);
+
+            return Content(json, "application/json");
+        }
+
+        [HttpPost("add")]
+        public ContentResult AddOrder([FromBody]AddOrderModel model)
+        {
+            try
+            {
+                Order order = new Order()
+                {
+                    ApartId = model.ApartId,
+                    Status = model.Status,
+                    UserId = model.UserId,
+                    RealtorId = model.RealtorId
+                };
+
+                _context.Orders.Add(order);
+                _context.SaveChanges();
+
+                ApiResponse APIResponse = new ApiResponse()
+                {
+                    Success = true,
+                    Result = "PLUS ORDER"
+                };
+                return Content(JsonConvert.SerializeObject(APIResponse), "application/json");
+            }
+            catch (Exception ex)
+            {
+                ApiResponse APIResponse = new ApiResponse()
+                {
+                    Success = false,
+                    ExceptionMessage = ex.Message
+                };
+                return Content(JsonConvert.SerializeObject(APIResponse), "application/json");
+            }
+        }
+
+
+    }
+}
---
+++ HomeRealtor/HomeRealtorApi/Models/AddOrderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeRealtorApi.Models
+{
+    public class AddOrderModel
+    {
+        public int ApartId { get; set; }
+
+        public bool Status { get; set; }
+
+        public string UserId { get; set; }
+
+        public string RealtorId { get; set; }
+    }
+}
