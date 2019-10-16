0d39024e7dd2810cac6b9a54062493daebcf4a9c
vetalkasmile <vetalkasmile@gmail.com>
2019-10-16 16:34:46 +0300

    Change name controller

---
+++ HomeRealtor/HomeRealtorApi/Controllers/UserApartmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeRealtorApi.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeRealtorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserApartmentController : ControllerBase
+    {
+        private readonly EFContext _context;
+        private readonly IHostingEnvironment _appEnvironment;
+        public UserApartmentController(EFContext context, IHostingEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+    }
+}
