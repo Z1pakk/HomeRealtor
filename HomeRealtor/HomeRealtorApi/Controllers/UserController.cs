d14c993a84cc5177578bb06403e7bfd70cbcfdbb
Nikita Nikitich <danik22122005@gmail.com>
2019-10-28 16:38:09 +0300

    + all done

--- HomeRealtor/HomeRealtorApi/Controllers/UserController.cs
+++ HomeRealtor/HomeRealtorApi/Controllers/UserController.cs
@@ -9,10 +9,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using HomeRealtorApi.Entities;
 using HomeRealtorApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace HomeRealtorApi.Controllers
 {
@@ -36,34 +38,33 @@ namespace HomeRealtorApi.Controllers
         [HttpPost("add")]
         public async Task<ActionResult<string>> Add([FromBody]UserModel User)
         {
-
             User user = new User()
             {
-               
+                UserName = User.UserName,
                 Email = User.Email,
                 Age = User.Age,
-                UserName = User.UserName,
-                PhoneNumber =User.PhoneNumber,
+                PhoneNumber = User.PhoneNumber,
                 FirstName = User.FirstName,
-                AboutMe=User.AboutMe,
+                AboutMe = User.AboutMe,
                 LastName = User.LastName
             };
 
             var result = await _userManager.CreateAsync(user, User.Password);
+            await _userManager.AddToRoleAsync(user, User.Role);
             if (result.Succeeded)
             {
                 return Ok();
             }
-            return  BadRequest();
+            return BadRequest();
         }
         [HttpPut("edit/{id}")]
-        public ContentResult Edit(string id,[FromBody]UserModel User)
+        public ContentResult Edit(string id, [FromBody]UserModel User)
         {
             try
             {
                 var edit = _context.Users.FirstOrDefault(t => t.Id == id);
-                edit.Image=User.Image;
-                edit.LastName = User.LastName;  
+                edit.Image = User.Image;
+                edit.LastName = User.LastName;
                 edit.PhoneNumber = User.PhoneNumber;
                 edit.UserName = User.UserName;
                 edit.FirstName = User.FirstName;
@@ -76,10 +77,41 @@ namespace HomeRealtorApi.Controllers
             catch (Exception ex)
             {
 
-                return Content( "Еррор:"+ex.Message);
+                return Content("Еррор:" + ex.Message);
 
             }
-            
+
+        }
+
+        [HttpGet("current")]
+        [Authorize]
+        public async Task<ContentResult> CurrentUser()
+        {
+            try
+            {
+                //_userManager.FindByNameAsync(this.User.Identity.Name);
+                User us = _context.Users.FirstOrDefault(t => t.UserName == this.User.Identity.Name);
+                string json = JsonConvert.SerializeObject(new UserInfoModel()
+                {
+                    FirstName = us.FirstName,
+                    LastName = us.LastName,
+                    AboutMe = us.AboutMe,
+                    Age = us.Age,
+                    Email = us.Email,
+                    Image = us.Image,
+                    PhoneNumber = us.PhoneNumber,
+                    Id = us.Id,
+                    UserName = us.UserName,
+                });
+
+                return Content(json);
+            }
+            catch (Exception ec)
+            {
+                return Content("Error: " + ec.Message);
+            }
+
+
         }
         [HttpGet("unlock/{code}")]
         public async Task<ActionResult<string>> UnlockUser(string code)
@@ -95,105 +127,45 @@ namespace HomeRealtorApi.Controllers
 
 
         [HttpPost("login")]
-        public async Task<ActionResult<string>> Login([FromBody]UserLoginModel loginModel)
+        public async Task<IActionResult> Login([FromBody]UserLoginModel loginModel)
         {
 
-            // User User = _context.Users.FirstOrDefault(t => t.Email == loginModel.Email);
-            
-            
-           try
-            {
-
-                User user  = await _userManager.FindByEmailAsync(loginModel.Email);
-                if (user == null)
-                {
-                    _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
-                    await _context.SaveChangesAsync();
-                    return "Error";
-                }
-                var result = await _sigInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
-            if (user.CountOfLogins >= 10)
-            {
-
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-                string code = Guid.NewGuid().ToString();
-                _context.UserUnlockCodes.Add(new UserUnlockCodes()
-                {
-                    Code = code,
-                    UserId = user.Id
-                });
-                mail.From = new MailAddress("home.realtor.suport@gmail.com");
-                mail.To.Add(user.Email);
-                mail.Subject = "Unlock account";
-                mail.IsBodyHtml = true;
-                mail.Body = "" +
-                "<head>" +
-                "Your account is locked press button to unlock :" +
-                "</head>"+
-                $" <a href=\" https://localhost:44325/api/user/unlock/{code}/ \">" +
-                "<button>" +
-                "Unlock" +
-                "</button>" +
-                " </a>  ";
-             
-
-
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("home.realtor.suport@gmail.com", "00752682");
-                SmtpServer.EnableSsl = true;
-                SmtpServer.Send(mail);
-    
-                await _userManager.SetLockoutEnabledAsync(user, true );
-                return "Locked";
-            }
-
-
-               // List<string> role =(List<string>)await _userManager.GetRolesAsync(user);
-            if(await _userManager.IsLockedOutAsync(user))
-            {
-                
-                return "Locked";
-            }
+            User user = await _userManager.FindByEmailAsync(loginModel.Email);
+            List<string> role=(List<string>)await _userManager.GetRolesAsync(user);
+            Thread.Sleep(5000);
             //TODO: FindByPhoneAsync
             //if(user==null)
             //{
             //    user= await _userManager.FindByPhoneAsync(loginModel.Email);
             //}
-            
-            
-            if (!result.Succeeded)
+            if (user == null)
             {
-                _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
-                await _context.SaveChangesAsync();
                 return "Error";
             }
-                _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins=0;
-                return   CreateTokenAsync(user/*,role[0]*/);
-            }
-            catch(Exception ex)
+            var result = await _sigInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
+            if (!result.Succeeded)
             {
-                _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
-                await _context.SaveChangesAsync();
                 return "Error";
             }
 
-          //  return  CreateTokenAsync(user,role[0]);
+            return CreateTokenAsync(user,role[0]);
                 
              
         }
 
-        private string CreateTokenAsync(User user/*,string role*/)
+        private string CreateTokenAsync(User user,string role)
         {
             List<Claim> claims = new List<Claim>()
             {
                 new Claim("id",user.Id),
-               // new Claim("role",role)
+                new Claim("role",role)
             };
             var now = DateTime.UtcNow;
             var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret-key-example"));
             var signinCredentials = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256);
             // Generate the jwt token
+            // tyt byv melnyk )))
+
             var jwt = new JwtSecurityToken(
                 signingCredentials: signinCredentials,
                 expires: now.Add(TimeSpan.FromDays(1)),
