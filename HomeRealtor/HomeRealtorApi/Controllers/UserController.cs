b2082c0854deda71536bec9ea8188031d7e064f2
Nikita Nikitich <danik22122005@gmail.com>
2019-10-29 16:34:16 +0200

    +

--- HomeRealtor/HomeRealtorApi/Controllers/UserController.cs
+++ HomeRealtor/HomeRealtorApi/Controllers/UserController.cs
@@ -9,12 +9,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using HomeRealtorApi.Entities;
 using HomeRealtorApi.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
 
 namespace HomeRealtorApi.Controllers
 {
@@ -38,33 +36,34 @@ namespace HomeRealtorApi.Controllers
         [HttpPost("add")]
         public async Task<ActionResult<string>> Add([FromBody]UserModel User)
         {
+
             User user = new User()
             {
-                UserName = User.UserName,
+               
                 Email = User.Email,
                 Age = User.Age,
-                PhoneNumber = User.PhoneNumber,
+                UserName = User.UserName,
+                PhoneNumber =User.PhoneNumber,
                 FirstName = User.FirstName,
-                AboutMe = User.AboutMe,
+                AboutMe=User.AboutMe,
                 LastName = User.LastName
             };
 
             var result = await _userManager.CreateAsync(user, User.Password);
-            await _userManager.AddToRoleAsync(user, User.Role);
             if (result.Succeeded)
             {
                 return Ok();
             }
-            return BadRequest();
+            return  BadRequest();
         }
         [HttpPut("edit/{id}")]
-        public ContentResult Edit(string id, [FromBody]UserModel User)
+        public ContentResult Edit(string id,[FromBody]UserModel User)
         {
             try
             {
                 var edit = _context.Users.FirstOrDefault(t => t.Id == id);
-                edit.Image = User.Image;
-                edit.LastName = User.LastName;
+                edit.Image=User.Image;
+                edit.LastName = User.LastName;  
                 edit.PhoneNumber = User.PhoneNumber;
                 edit.UserName = User.UserName;
                 edit.FirstName = User.FirstName;
@@ -77,41 +76,10 @@ namespace HomeRealtorApi.Controllers
             catch (Exception ex)
             {
 
-                return Content("Еррор:" + ex.Message);
+                return Content( "Еррор:"+ex.Message);
 
             }
-
-        }
-
-        [HttpGet("current")]
-        [Authorize]
-        public async Task<ContentResult> CurrentUser()
-        {
-            try
-            {
-                //_userManager.FindByNameAsync(this.User.Identity.Name);
-                User us = _context.Users.FirstOrDefault(t => t.UserName == this.User.Identity.Name);
-                string json = JsonConvert.SerializeObject(new UserInfoModel()
-                {
-                    FirstName = us.FirstName,
-                    LastName = us.LastName,
-                    AboutMe = us.AboutMe,
-                    Age = us.Age,
-                    Email = us.Email,
-                    Image = us.Image,
-                    PhoneNumber = us.PhoneNumber,
-                    Id = us.Id,
-                    UserName = us.UserName,
-                });
-
-                return Content(json);
-            }
-            catch (Exception ec)
-            {
-                return Content("Error: " + ec.Message);
-            }
-
-
+            
         }
         [HttpGet("unlock/{code}")]
         public async Task<ActionResult<string>> UnlockUser(string code)
@@ -127,7 +95,7 @@ namespace HomeRealtorApi.Controllers
 
 
         [HttpPost("login")]
-        public async Task<ActionResult<string>> Login([FromBody]UserLoginModel loginModel)
+        public async Task<IActionResult> Login([FromBody]UserLoginModel loginModel)
         {
 
             User user = await _userManager.FindByEmailAsync(loginModel.Email);
@@ -145,6 +113,8 @@ namespace HomeRealtorApi.Controllers
             var result = await _sigInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
             if (!result.Succeeded)
             {
+                _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
+                await _context.SaveChangesAsync();
                 return "Error";
             }
 
@@ -153,19 +123,17 @@ namespace HomeRealtorApi.Controllers
              
         }
 
-        private string CreateTokenAsync(User user,string role)
+        private string CreateTokenAsync(User user/*,string role*/)
         {
             List<Claim> claims = new List<Claim>()
             {
                 new Claim("id",user.Id),
-                new Claim("role",role)
+               // new Claim("role",role)
             };
             var now = DateTime.UtcNow;
             var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret-key-example"));
             var signinCredentials = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256);
             // Generate the jwt token
-            // tyt byv melnyk )))
-
             var jwt = new JwtSecurityToken(
                 signingCredentials: signinCredentials,
                 expires: now.Add(TimeSpan.FromDays(1)),
