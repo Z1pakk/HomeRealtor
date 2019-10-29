b9e53a9e32ea1b1976e0fcdc819c95bd84d94e44
Nikita Nikitich <danik22122005@gmail.com>
2019-10-29 16:34:38 +0200

    +

--- HomeRealtor/HomeRealtorApi/Controllers/UserController.cs
+++ HomeRealtor/HomeRealtorApi/Controllers/UserController.cs
@@ -98,29 +98,89 @@ namespace HomeRealtorApi.Controllers
         public async Task<IActionResult> Login([FromBody]UserLoginModel loginModel)
         {
 
-            User user = await _userManager.FindByEmailAsync(loginModel.Email);
-            List<string> role=(List<string>)await _userManager.GetRolesAsync(user);
-            Thread.Sleep(5000);
-            //TODO: FindByPhoneAsync
-            //if(user==null)
-            //{
-            //    user= await _userManager.FindByPhoneAsync(loginModel.Email);
-            //}
-            if (user == null)
+            // User User = _context.Users.FirstOrDefault(t => t.Email == loginModel.Email);
+
+
+            try
             {
-                return "Error";
+
+                User user = await _userManager.FindByEmailAsync(loginModel.Email);
+                if (user == null)
+                {
+                    _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
+                    await _context.SaveChangesAsync();
+                    return "Error";
+                }
+                var result = await _sigInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
+                if (user.CountOfLogins >= 10)
+                {
+
+                    MailMessage mail = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                    string code = Guid.NewGuid().ToString();
+                    _context.UserUnlockCodes.Add(new UserUnlockCodes()
+                    {
+                        Code = code,
+                        UserId = user.Id
+                    });
+                    mail.From = new MailAddress("home.realtor.suport@gmail.com");
+                    mail.To.Add(user.Email);
+                    mail.Subject = "Unlock account";
+                    mail.IsBodyHtml = true;
+                    mail.Body = "" +
+                    "<head>" +
+                    "Your account is locked press button to unlock :" +
+                    "</head>" +
+                    $" <a href=\" https://localhost:44325/api/user/unlock/{code}/ \">" +
+                    "<button>" +
+                    "Unlock" +
+                    "</button>" +
+                    " </a>  ";
+
+
+
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("home.realtor.suport@gmail.com", "00752682");
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(mail);
+
+                    await _userManager.SetLockoutEnabledAsync(user, true);
+                    return "Locked";
+                }
+
+
+                // List<string> role =(List<string>)await _userManager.GetRolesAsync(user);
+                if (await _userManager.IsLockedOutAsync(user))
+                {
+
+                    return "Locked";
+                }
+                //TODO: FindByPhoneAsync
+                //if(user==null)
+                //{
+                //    user= await _userManager.FindByPhoneAsync(loginModel.Email);
+                //}
+
+
+                if (!result.Succeeded)
+                {
+                    _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
+                    await _context.SaveChangesAsync();
+                    return "Error";
+                }
+                _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins = 0;
+                return CreateTokenAsync(user/*,role[0]*/);
             }
-            var result = await _sigInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
-            if (!result.Succeeded)
+            catch (Exception ex)
             {
                 _context.Users.FirstOrDefault(t => t.Email == loginModel.Email).CountOfLogins++;
                 await _context.SaveChangesAsync();
                 return "Error";
             }
 
-            return CreateTokenAsync(user,role[0]);
-                
-             
+            //  return  CreateTokenAsync(user,role[0]);
+
+
         }
 
         private string CreateTokenAsync(User user/*,string role*/)
